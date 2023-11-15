dcd898b9f8b0cfbb953fe4cc2355ba3618c4e697
iShantaram <115453774+iShantaram@users.noreply.github.com>
2023-11-15 18:44:41 +0300

    Создана реализация ArrListImpl.
    Для работы с листами Integer.
    Создано три вида сортировок ArrListImpl - пузырьком(bubble), максимумом(maximum) и быстрая(qSort).
    На массиве из 100_000 элементов время сортировок примерно следующее:
    пузырек - более минуты, максимумом - 15 сек, qSort - менее секунды.
    Статический метод qSort создан для проверки времени работы метода быстрой сортировки.
    Приватный qSort работает с внутренним массивом перед бинарным поиском элемента.
    Тестами покрыты все методы кроме qSort, так как она используется внутри binaryContains, а тест на binaryContains в себе это учитывает.

--- Main.java
+++ Main.java
@@ -1,7 +1,52 @@
 package pro.sky.agapov;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int count = 100_000;
+        ArrListImpl arrList = new ArrListImpl(count);
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            arrList.add(random.nextInt(-100_000, 100_000));
+        }
+
+        ArrListImpl arrListCopy1 = new ArrListImpl(arrList.toArray());
+        ArrListImpl arrListCopy2 = new ArrListImpl(arrList.toArray());
+        ArrListImpl arrListCopy3 = new ArrListImpl(arrList.toArray());
+
+        long start = System.currentTimeMillis();
+        arrListCopy1.sortBubble();
+        System.out.print("Bubble time = ");
+        System.out.println(System.currentTimeMillis() - start);
+
+        long start2 = System.currentTimeMillis();
+        arrListCopy2.sortMaximum();
+        System.out.print("Maximum time = ");
+        System.out.println(System.currentTimeMillis() - start2);
+
+        long start3 = System.currentTimeMillis();
+        Integer[] sortedArray = ArrListImpl.qSort(arrListCopy3.toArray(), 0, arrListCopy3.size()-1);
+        System.out.print("QSort time = ");
+        System.out.println(System.currentTimeMillis() - start3);
+        System.out.println(Arrays.toString(sortedArray));
+
+        System.out.println("Обычный:");
+        if (arrList.contains(0)) {
+            System.out.println("0 содержит");
+            System.out.println("0 - " + arrList.indexOf(0) + "-ый элемент");
+        } else {
+            System.out.println("0 не содержит");
+        }
+
+        // После использования метода binaryContains внутренний массив будет отсортирован приватным методом qSort
+        System.out.println("Бинарный:");
+        if (arrList.binaryContains(0)) {
+            System.out.println("0 содержит");
+            System.out.println("0 - " + arrList.indexOf(0) + "-ый элемент в упорядоченном массиве");
+        } else {
+            System.out.println("0 не содержит");
+        }
     }
 }
